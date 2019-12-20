b9ceda9ff5012d16dfb96aece1369a514d8d17ab
Philip917 <54669419+Philip917@users.noreply.github.com>
2019-12-20 14:50:11 +0200

    Static files for website

---
+++ precache-manifest.83de896b31099e8debf286189433970c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b2a5ed9c0810d8fb8e41c34eaff9062",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "71a3fbc42556f1467c7e",
+    "url": "/portfolio/static/css/main.5317293d.chunk.css"
+  },
+  {
+    "revision": "fce24a4baa6a695afca8",
+    "url": "/portfolio/static/js/2.b9f323cf.chunk.js"
+  },
+  {
+    "revision": "71a3fbc42556f1467c7e",
+    "url": "/portfolio/static/js/main.ba250fe5.chunk.js"
+  },
+  {
+    "revision": "791f997dc0acbb20fb93",
+    "url": "/portfolio/static/js/runtime-main.021c856b.js"
+  },
+  {
+    "revision": "a0fd09e252e724bb523cc68bc147ed3a",
+    "url": "/portfolio/static/media/b20.a0fd09e2.jpeg"
+  }
+]);
